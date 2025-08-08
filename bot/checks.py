259495efa76c6b11ed29f5dc46b375bc427ba021
Tagninolpi <tagnindesbois@gmail.com>
2025-08-08 15:34:14 +0200

    ch

--- bot/checks.py
+++ bot/checks.py
@@ -9,13 +9,13 @@ class Check():
         async def predicate(interaction: Interaction) -> bool:
             channel = interaction.channel
             if not isinstance(channel, TextChannel):
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     f"❌ This command must be used in a text channel (`{Config.LOBBY_CATEGORY_NAME}`).",
                     ephemeral=True
                )
                 return False
             if not channel.category or channel.category.name != Config.LOBBY_CATEGORY_NAME:
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     f"❌ You must use this command in a `{Config.LOBBY_CATEGORY_NAME}` channel.",
                     ephemeral=True
                 )
@@ -28,7 +28,7 @@ class Check():
         async def predicate(interaction: Interaction) -> bool:
             match = matchmaking_dict.get(interaction.channel.id)
             if not match or interaction.user.id not in [match.player1_id, match.player2_id]:
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     "🚫 You are not a participant in this match.",
                     ephemeral=True
                 )
@@ -40,7 +40,7 @@ class Check():
         @check
         async def predicate(interaction: Interaction) -> bool:
             if interaction.channel.id != allowed_channel_id:
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     "❌ You can't use this command here.",
                     ephemeral=True
                 )
@@ -53,7 +53,7 @@ class Check():
         async def predicate(interaction: Interaction) -> bool:
             match = matchmaking_dict.get(interaction.channel.id)
             if not match or match.game_phase != required_phase:
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     f"You can't use this command now (required: {required_phase}).",
                     ephemeral=True
                 )
@@ -66,7 +66,7 @@ class Check():
         async def predicate(interaction: Interaction) -> bool:
             match = matchmaking_dict.get(interaction.channel.id)
             if match and match.turn_in_progress:
-                await interaction.followup.send(
+                await interaction.response.send_message(
                     "⏳ A turn is already in progress. Please choose a god.",
                     ephemeral=True
                 )
