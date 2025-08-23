04091945f5cd54b0f05f649a16c6aec14c023d2b
Tagninolpi <tagnindesbois@gmail.com>
2025-08-23 20:44:22 +0200

    again

--- cogs/turn.py
+++ cogs/turn.py
@@ -13,7 +13,7 @@ import re
 import unicodedata
 from bot.config import Config
 
-from bot_AI_functions.bot_class import BotClass
+from .bot_AI_functions.bot_class import BotClass
 
 
 logger = logging.getLogger(__name__)
