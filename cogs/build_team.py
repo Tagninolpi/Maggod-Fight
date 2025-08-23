d1c220c734af33e37c2a4cfef03637af712f1140
Tagninolpi <tagnindesbois@gmail.com>
2025-08-23 21:15:59 +0200

    import verif

--- cogs/build_team.py
+++ cogs/build_team.py
@@ -12,7 +12,7 @@ from bot.utils import update_lobby_status_embed
 from bot.config import Config
 import asyncio
 
-from bot_AI_functions.bot_class import BotClass,bot_configs
+from ..bot_AI_functions.bot_class import BotClass,bot_configs
 
 logger = logging.getLogger(__name__)
 class StartChoiceView(discord.ui.View):
